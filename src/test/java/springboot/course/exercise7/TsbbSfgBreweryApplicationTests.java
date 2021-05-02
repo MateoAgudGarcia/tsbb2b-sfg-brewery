94dbc04f53d854f1fff954e24749cc2d38322590
Mateo Agudelo Garcia <mateo.agudelog@perficient.com>
2021-05-02 15:47:35 -0500

    Configure downloaded project

---
+++ src/test/java/springboot/course/exercise7/TsbbSfgBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.course.exercise7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TsbbSfgBreweryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
